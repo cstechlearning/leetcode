2f887f4a1691607714b72327ec13e5c6865041b7
Aaron Wu <pwu@brown.edu>
2017-03-29 02:12:20 -0400

    Create 530. Min_diff_BST__follow-up.java

---
+++ 530. Min_diff_BST__follow-up.java
@@ -0,0 +1,40 @@
+// find the minimum absolute difference between values of two "adjacent" nodes.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Solution {
+    private ArrayList<Integer> diff = new ArrayList<>();
+	
+	public int getMinimumDifference(TreeNode root) {
+		// pre
+		if(root==null)   // null, it will return, so no worry getMini() needs if(r.left=!null)
+			return Collections.min( diff );
+			
+		else{
+			if(root.left!=null){
+				diff.add( Math.abs(root.val - root.left.val) );
+			}
+
+			if(root.right!=null){
+				diff.add( Math.abs(root.val - root.right.val) );
+			}
+
+			getMinimumDifference( root.left );
+			getMinimumDifference( root.right );
+		}
+		
+		return Collections.min( diff );
+			
+	}
+}
