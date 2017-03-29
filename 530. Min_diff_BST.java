4dc58cd74a9b55e969c520e01388ae159e2e3a68
Aaron Wu <pwu@brown.edu>
2017-03-29 13:25:11 -0400

    Create 530. Min_diff_BST.java

---
+++ 530. Min_diff_BST.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Arrays;
+
+public class Solution {
+	private ArrayList<Integer> vals = new ArrayList<>();
+	
+    public int getMinimumDifference(TreeNode root) {
+    	getVals(root);
+    
+    	return getMin();
+    }
+
+    public void getVals(TreeNode root){
+    	if(root==null){
+    		return;
+    	} else {
+    		vals.add(root.val);
+
+    		getVals( root.left  );
+    		getVals( root.right );
+    	}
+    	return;
+    }
+
+    public int getMin(){
+    	int[] diff = new int[vals.size()-1];
+
+    	Collections.sort(vals);
+
+    	for(int i=0; i<vals.size()-1; i++){
+    		diff[i] =  Math.abs( vals.get(i+1) - vals.get(i) );
+    	}
+
+    	Arrays.sort(diff);
+
+    	return diff[0];
+
+
+    }
+
+}
